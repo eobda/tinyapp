5e2fcabc5b2286302071b87679c494cfc20ef6d3
maddie <lee.maddie@gmail.com>
2023-10-01 13:41:18 -0400

    Fix spacing error

--- express_server.js
+++ express_server.js
@@ -49,7 +49,6 @@ app.get('/urls', (req, res) => {
 app.get('/urls/new', (req, res) => {
   const templateVars = { 
     username: req.cookies['username']
-  
   };
   res.render('urls_new', templateVars);
 });
