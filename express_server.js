109184da684f26f40349a2c73a87a3f0fc372fae
maddie <lee.maddie@gmail.com>
2023-10-02 20:14:35 -0400

    Create global users object

--- express_server.js
+++ express_server.js
@@ -12,6 +12,19 @@ const urlDatabase = {
   '9sm5xK': 'http://www.google.com'
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
 // Return a string of 6 random alphanumeric characters
 function generateRandomString() {
   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
