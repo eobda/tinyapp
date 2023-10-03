a018ae41907fa5d778e7438189a1b7a815e11735
maddie <lee.maddie@gmail.com>
2023-10-03 11:23:50 -0400

    Redirect logout route to login route

--- express_server.js
+++ express_server.js
@@ -85,7 +85,7 @@ app.post('/login', (req, res) => {
 
 app.post('/logout', (req, res) => {
   res.clearCookie('user_id');
-  res.redirect('/urls');
+  res.redirect('/login');
 });
 
 app.get('/urls.json', (req, res) => {
