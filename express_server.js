bb9a60419a1d42127034c0e13196071f6e361174
maddie <lee.maddie@gmail.com>
2023-10-06 14:09:43 -0400

    Remove cookie-parser

--- express_server.js
+++ express_server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 const bcrypt = require('bcryptjs');
-// const cookieParser = require('cookie-parser');
 const cookieSession = require('cookie-session');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
 app.use(cookieSession({
   name: 'session',
   keys: ['key1', 'key2']
